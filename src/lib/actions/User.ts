aa3cf06d72025ca0e25667d1c92442f976862c2b
Tcoco4 <taremautsa@yahoo.com>
2025-07-24 22:30:16 +0200

    getUser server action

---
+++ src/lib/actions/User.ts
@@ -0,0 +1,34 @@
+import prisma from "@/repository/prisma";
+import crypto from "crypto";
+import { AuthenticatedUser } from "./types";
+
+export async function getUser(
+  email: string,
+  password: string
+): Promise<AuthenticatedUser | null> {
+  const salt = process.env["NEXTAUTH_SECRET"] ?? "salty";
+  const saltedPassword = password + salt;
+
+  const saltedAndHashedPassword = crypto
+    .createHash("sha512")
+    .update(saltedPassword)
+    .digest("base64");
+
+  const authorizedUser = await prisma.authorizedUser.findFirst({
+    where: {
+      email: email,
+      password: saltedAndHashedPassword,
+    },
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+
+  if (!authorizedUser) return null;
+
+  return {
+    name: authorizedUser.username,
+    id: authorizedUser.id.toString(),
+  };
+}
