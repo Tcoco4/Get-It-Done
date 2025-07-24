8a034b8a1de2308fd731754813ce9a6a908dbc41
Tcoco4 <taremautsa@yahoo.com>
2025-07-24 13:17:46 +0200

    add dashboard, navigationf

---
+++ src/lib/constants/router.ts
@@ -0,0 +1,10 @@
+export const NAVIGATION = [
+  {
+    href: "/dashboard",
+    icon: "",
+    label: "Dashboard",
+    tooltipDescription: "Task Dashboard",
+    matchPath: (pathname: string) =>
+      pathname === "/dashboard" || pathname === "/",
+  },
+];
