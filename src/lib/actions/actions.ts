e6751c10f8a2df0f7ef2d6379e1b73779296ab65
Tcoco4 <taremautsa@yahoo.com>
2025-07-25 22:24:00 +0200

    server actions created

--- src/lib/actions/actions.ts
+++ src/lib/actions/actions.ts
@@ -1,4 +1,6 @@
-import { List, Task } from "../types";
+import { AuthenticatedUser, List, Task } from "../types";
+import prisma from "../prisma-client/prisma";
+import crypto from "crypto";
 
 //Lists
 export const getAllLists = async (id: number) => {
@@ -18,3 +20,38 @@ export const addTask = async (task: Task, listId: number) => {};
 export const getTask = async (id: number) => {};
 export const deleteTask = async (taskId: number, listId: number) => {};
 export const updateTask = async (id: number) => {};
+
+//User
+
+export const addUser = async (email: string) => {};
+
+export const getUser = async (
+  email: string,
+  password: string
+): Promise<AuthenticatedUser | null> => {
+  const salt = process.env["NEXTAUTH_SECRET"] ?? "salty";
+  const saltedPassword = password + salt;
+
+  const saltedAndHashedPassword = crypto
+    .createHash("sha512")
+    .update(saltedPassword)
+    .digest("base64");
+
+  const authorizedUser = await prisma.authorizedUser.findFirst({
+    where: {
+      email: email,
+      password: saltedAndHashedPassword,
+    },
+    select: {
+      id: true,
+      email: true,
+    },
+  });
+
+  if (!authorizedUser) return null;
+
+  return {
+    name: authorizedUser.email,
+    id: authorizedUser.id.toString(),
+  };
+};
