037535ab9e964fa924517f726c9519bfa9d8e529
Tcoco4 <taremautsa@yahoo.com>
2025-07-25 23:25:36 +0200

    server actions complete

--- src/lib/actions/actions.ts
+++ src/lib/actions/actions.ts
@@ -1,29 +1,127 @@
 import { AuthenticatedUser, List, Task } from "../types";
 import prisma from "../prisma-client/prisma";
 import crypto from "crypto";
+import { select } from "@heroui/react";
 
 //Lists
-export const getAllLists = async (id: number) => {
-  return [{ title: "", total: 0, completed: 0, tasks: [] }] as unknown as List;
+export const getAllLists = async (id: number): Promise<List[]> => {
+  return await prisma.list.findMany({
+    where: { userId: id, auditStatus: "active" },
+  });
+  //   return [{ title: "", total: 0, completed: 0, tasks: [] }] as unknown as List;
 };
 
-export const createList = async () => {};
+export const createList = async (
+  list: List,
+  userId: number
+): Promise<number> => {
+  const newList = await prisma.list.create({
+    data: {
+      name: list.title,
+      total: list.total,
+      completed: list.completed,
+      tasks: [],
+      auditStatus: "active",
+      userId: userId,
+    },
+    select: { id: true },
+  });
+  return newList.id;
+};
 
-export const getList = async (id: number) => {};
+export const getList = async (id: number): Promise<List[] | Error> => {
+  const list = await prisma.list.findFirst({
+    where: { id: id },
+  });
 
-export const updateList = async (id: number) => {};
+  if (!list) throw new Error("id does not exist");
+  return list;
+};
 
-export const deleteList = async (id: number) => {};
+export const deleteList = async (id: number): Promise<boolean | Error> => {
+  const list = await prisma.list
+    .update({
+      where: { id: id },
+      data: { auditStatus: "deleted" },
+      select: { auditStatus: true },
+    })
+    .catch((err: any) => console.error(err));
+
+  if (!list) throw new Error(" id does not exist");
+  return list.auditStatus == "deleted" ? true : false;
+};
 
 //Tasks
-export const addTask = async (task: Task, listId: number) => {};
-export const getTask = async (id: number) => {};
-export const deleteTask = async (taskId: number, listId: number) => {};
-export const updateTask = async (id: number) => {};
+export const addTask = async (task: Task, listId: number): Promise<number> => {
+  const newTask = await prisma.task
+    .create({
+      data: {
+        title: task.name,
+        completed: task.complete,
+        taskDue: task.taskDue,
+        additional: task.additionalInformation,
+        auditStatus: "active",
+        list: {
+          connect: { id: listId },
+        },
+      },
+    })
+    .catch((err: any) => console.error(err));
+  return newTask.id;
+};
+export const getTask = async (id: number): Promise<Task | Error> => {
+  const task = await prisma.task.findFirst({
+    where: { id: id, auditStatus: "active" },
+  });
+  if (!task) throw new Error(" id does not exist");
+  return task;
+};
+export const deleteTask = async (
+  taskId: number,
+  listId: number
+): Promise<boolean> => {
+  const task = await prisma.task
+    .update({
+      where: { id: taskId, listId: listId },
+      data: { auditStatus: "deleted" },
+    })
+    .catch((err: any) => console.error(err));
+
+  return task.auditStatus == "deleted" ? true : false;
+};
+export const updateTaskStatus = async (
+  id: number,
+  status: boolean
+): Promise<boolean> => {
+  const updatedTaskStatus = await prisma.task.update({
+    where: { id: id },
+    data: { complete: status },
+  });
+
+  return updatedTaskStatus.complete == status ? true : false;
+};
+export const updateTaskTitle = async (
+  id: number,
+  title: string
+): Promise<boolean> => {
+  const updatedTaskName = await prisma.task.update({
+    where: { id: id },
+    data: { name: title },
+  });
+  return updatedTaskName.name == title ? true : false;
+};
 
 //User
 
-export const addUser = async (email: string) => {};
+export const addUser = async (email: string): Promise<boolean> => {
+  const user = await prisma.User.create({
+    data: {
+      email: email,
+      list: [],
+    },
+  });
+  return user ? true : false;
+};
 
 export const getUser = async (
   email: string,
